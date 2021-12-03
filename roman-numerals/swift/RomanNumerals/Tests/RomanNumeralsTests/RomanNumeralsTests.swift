13bed90d1ea7fcd84f64502ee3d8982979bc0df3
Sarah Lichter <lichter.sarahe@gmail.com>
2021-12-03 10:05:24 -0600

    added swift roman numerals starter project

---
+++ roman-numerals/swift/RomanNumerals/Tests/RomanNumeralsTests/RomanNumeralsTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import RomanNumerals
+
+final class RomanNumeralsTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(RomanNumerals().text, "Nope")
+    }
+}
