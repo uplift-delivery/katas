271cc89881b21fcf83820f6a2b36c0e0cdbce686
Natalie Frecka <natalie.frecka@uplift.delivery.com>
2023-11-07 11:30:54 -0600

    adding empty kotlin skeleton to fizzbuzz dir

---
+++ fizzbuzz/kotlin/src/test/kotlin/com/uplift_delivery/fizzbuzz/FizzbuzzTest.kt
@@ -0,0 +1,11 @@
+package com.uplift_delivery.fizzbuzz
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class FizzbuzzTest {
+    @Test
+    fun `is passing`() {
+        assertEquals(0 ,0)
+    }
+}
