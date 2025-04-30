c22b93423d18ac9b314b281e9f65cf3434f3549f
justin81co <justin81red@gmail.com>
2025-04-30 09:58:03 -0600

    Update Claim.tests.ts

--- insurance/src/tests/Claim.tests.ts
+++ insurance/src/tests/Claim.tests.ts
@@ -4,3 +4,10 @@ const exampleClaim = {
   incidentDate: new Date('2023-06-15'),
   amountClaimed: 3000,
 };
+/* test if there is a policy
+test if the incident type is a valid type and is there
+test if date is there and is formatted correctly
+test the amount is a number and not zero or negitive
+test the return to see if it returns reason code and approved boolean and otional value
+test bad policy string
+test if policy 
